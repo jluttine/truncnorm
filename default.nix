6efa0dab216301a40ba4b5eb29891c04ce681dcd
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2024-02-28 12:52:46 +0200

    Initial working version for single truncation

---
+++ default.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> { };
+  ps = pkgs.python3Packages;
+in
+ps.buildPythonPackage rec {
+  name = "truncnorm";
+  doCheck = false;
+  src = ./.;
+  postShellHook = ''
+    export PYTHONPATH=$(pwd):$PYTHONPATH
+  '';
+  depsBuildBuild = with ps; [
+    ipython
+    pip
+    twine
+    setuptools_scm
+    pkgs.git
+  ];
+  propagatedBuildInputs = with ps; [
+    numpy
+    scipy
+  ];
+
+}
